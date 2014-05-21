94cfc7a74b8e4bf50e79b1941fe7e12b79ab226d
ELC201203 <contact@namsor.com>
2014-05-21 12:48:08 +0200

    Beta v0.0.1
    Beta v0.0.1

--- src/com/namsor/api/rapidminer/GendREOperator.java
+++ src/com/namsor/api/rapidminer/GendREOperator.java
@@ -31,31 +31,49 @@ import com.rapidminer.parameter.ParameterTypeDouble;
 import com.rapidminer.parameter.ParameterTypeString;
 import com.rapidminer.tools.Ontology;
 
-public class Name2GenderOperator extends Operator {
-	private static final boolean MOCKUP = true;
+/**
+ * Onomastics is a set of operators to extract information from personal names.
+ * - GendRE Genderizer is an operator to infer the gender from international
+ * names. GendRE already covers many languages/cultures/geographies (USA,
+ * Europe, Cyrillic Russia/CIS, Hebrew, Arabic/the Arab World, Chinese, ...).
+ * Possible usage include: gender studies, public policies monitoring (gender
+ * equality, etc.), customer intelligence... The online API is available with a
+ * Free, Freemium and Premium model, with the Free model covering ~80% of
+ * general needs.
+ * 
+ * @author ELC201203
+ * 
+ */
+public class GendREOperator extends Operator {
+	private static final boolean MOCKUP = false;
 	private static final Random RND = new Random();
-	private static final String MASHAPE_API_ADDRESS = "https://namsor-gendre.p.mashape.com/gendre/";
+
+	private static final String API_CHANNEL_SECRET = "APIKey";
+	private static final String API_CHANNEL_USER = "APIChannel";
+
+	private static final String MASHAPE_CHANNEL_USER = "mashape.com";
 
 	private static final String ATTRIBUTE_THRESHOLD = "threshold";
-	private static final Double ATTRIBUTE_THRESHOLD_DEFAULT = .2d;
+	private static final Double ATTRIBUTE_THRESHOLD_DEFAULT = .1d;
 	private static final String ATTRIBUTE_FN = "firstName";
 	private static final String ATTRIBUTE_LN = "lastName";
 	private static final String ATTRIBUTE_ISO2 = "countryIso2";
+	private static final String ATTRIBUTE_BATCHID = "batchId";
+
 	private static final String ATTRIBUTE_GENDERSCALE = "genderScale";
 	private static final String ATTRIBUTE_GENDER = "gender";
 	private static final String DEFAULT_COUNTRY_ISO2 = "defaultCountryIso2";
-	private static final String MASHAPE_API_KEY = "mashapeAPIKey";
 	private InputPort inputSet = getInputPorts().createPort("nameSet");
 	private OutputPort outputSet = getOutputPorts().createPort(
 			"nameAndGenderSet");
 
-	public Name2GenderOperator(OperatorDescription description) {
+	public GendREOperator(OperatorDescription description) {
 		super(description);
 		inputSet.addPrecondition(new ExampleSetPrecondition(inputSet,
 				new String[] { ATTRIBUTE_FN }, Ontology.ATTRIBUTE_VALUE));
 		inputSet.addPrecondition(new ExampleSetPrecondition(inputSet,
 				new String[] { ATTRIBUTE_LN }, Ontology.ATTRIBUTE_VALUE));
-		//getTransformer().addPassThroughRule(inputSet, outputSet);
+		// getTransformer().addPassThroughRule(inputSet, outputSet);
 	}
 
 	@Override
@@ -66,16 +84,18 @@ public class Name2GenderOperator extends Operator {
 		Attribute fnAttribute = attributes.get(ATTRIBUTE_FN);
 		Attribute lnAttribute = attributes.get(ATTRIBUTE_LN);
 		Attribute iso2Attribute = attributes.get(ATTRIBUTE_ISO2);
+		Attribute batchIdAttribute = attributes.get(ATTRIBUTE_BATCHID);
 
-		String mashapeAPIKey = getParameterAsString(MASHAPE_API_KEY);
+		String APIKey = getParameterAsString(API_CHANNEL_SECRET);
+		String APIChannel = getParameterAsString(API_CHANNEL_USER);
 		String defaultISO2 = getParameterAsString(DEFAULT_COUNTRY_ISO2);
 		double threshold = getParameterAsDouble(ATTRIBUTE_THRESHOLD);
 
 		Attribute genderScaleAttribute = AttributeFactory.createAttribute(
-				ATTRIBUTE_GENDERSCALE, Ontology.REAL);		
+				ATTRIBUTE_GENDERSCALE, Ontology.REAL);
 		exampleSet.getExampleTable().addAttribute(genderScaleAttribute);
 		attributes.addRegular(genderScaleAttribute);
-		
+
 		Attribute genderAttribute = AttributeFactory.createAttribute(
 				ATTRIBUTE_GENDER, Ontology.STRING);
 		exampleSet.getExampleTable().addAttribute(genderAttribute);
@@ -84,7 +104,14 @@ public class Name2GenderOperator extends Operator {
 		for (Example example : exampleSet) {
 			String firstName = example.getValueAsString(fnAttribute);
 			String lastName = example.getValueAsString(lnAttribute);
-			String iso2 = example.getValueAsString(iso2Attribute);
+			String iso2 = null;
+			if (iso2Attribute != null) {
+				iso2 = example.getValueAsString(iso2Attribute);
+			}
+			String batchId = null;
+			if (batchIdAttribute != null) {
+				batchId = example.getValueAsString(batchIdAttribute);
+			}
 			if (iso2 != null && iso2.trim().length() == 2) {
 				// real value
 			} else if (defaultISO2 != null && defaultISO2.trim().length() == 2) {
@@ -93,62 +120,47 @@ public class Name2GenderOperator extends Operator {
 				// invalid value, set to null
 				iso2 = null;
 			}
-
 			double genderScale = 0d;
 			if (MOCKUP) {
 				genderScale = RND.nextDouble() * 2 - 1;
 			} else {
 				try {
-					if (mashapeAPIKey == null || mashapeAPIKey.trim().isEmpty()) {
+					if (APIKey == null || APIKey.trim().isEmpty()) {
 						// use Free API
-						genderScale = GenderAPIClient.getInstance().genderize(
-								firstName, lastName, iso2);
+						genderScale = GenderAPIClient.getInstance()
+								.genderizeFree(firstName, lastName, iso2);
 						Logger.getLogger(getClass().getName()).log(
-								Level.INFO,
+								Level.FINE,
 								"GendRE API Free " + firstName + "/" + lastName
 										+ "/" + iso2 + "=" + genderScale);
-					} else {
+					} else if (APIChannel != null
+							&& APIChannel.trim().toLowerCase()
+									.startsWith(MASHAPE_CHANNEL_USER)) {
 						// use Mashape API
-						String url = null;
-						if (iso2 == null) {
-							url = MASHAPE_API_ADDRESS
-									+ URLEncoder.encode(
-											firstName.replace('.', ' ').trim(),
-											"UTF-8")
-									+ "/"
-									+ URLEncoder.encode(
-											lastName.replace('.', ' ').trim(),
-											"UTF-8");
-						} else {
-							url = MASHAPE_API_ADDRESS
-									+ URLEncoder.encode(
-											firstName.replace('.', ' ').trim(),
-											"UTF-8")
-									+ "/"
-									+ URLEncoder.encode(
-											lastName.replace('.', ' ').trim(),
-											"UTF-8") + "/" + iso2;
-						}
-						HttpResponse<String> request = Unirest
-								.get(url)
-								.header("X-Mashape-Authorization",
-										mashapeAPIKey).asString();
-						genderScale = Double.parseDouble(request.getBody());
+						genderScale = GenderAPIClient.getInstance()
+								.genderizeMashape(APIKey, firstName, lastName,
+										iso2);
 						Logger.getLogger(getClass().getName()).log(
-								Level.INFO,
-								"GendRE API Freemium " + firstName + "/"
+								Level.FINE,
+								"GendRE API Freemium (Mashape) " + firstName
+										+ "/" + lastName + "/" + iso2 + "="
+										+ genderScale);
+					} else if (APIChannel != null
+							&& !APIChannel.trim().isEmpty() && APIKey != null
+							&& !APIKey.trim().isEmpty()) {
+						// use Premium API
+						genderScale = GenderAPIClient.getInstance()
+								.genderizePremium(APIChannel, APIKey, batchId,
+										firstName, lastName, iso2);
+						Logger.getLogger(getClass().getName()).log(
+								Level.FINE,
+								"GendRE API Premium " + firstName + "/"
 										+ lastName + "/" + iso2 + "="
 										+ genderScale);
 					}
 				} catch (GenderAPIException e) {
 					Logger.getLogger(getClass().getName()).log(Level.SEVERE,
 							"GenderAPI error : " + e.getMessage(), e);
-				} catch (UnirestException e) {
-					Logger.getLogger(getClass().getName()).log(Level.SEVERE,
-							"GenderAPI/Mashape error : " + e.getMessage(), e);
-				} catch (IOException e) {
-					Logger.getLogger(getClass().getName()).log(Level.SEVERE,
-							"IO error : " + e.getMessage(), e);
 				}
 			}
 			String gender = "Unknown";
@@ -167,16 +179,18 @@ public class Name2GenderOperator extends Operator {
 	public List<ParameterType> getParameterTypes() {
 		List<ParameterType> types = super.getParameterTypes();
 		types.add(new ParameterTypeString(
-				MASHAPE_API_KEY,
-				"Insert your Mashape API Key here for the Freemium version, best precision and better support (Free version is used otherwise).",
+				API_CHANNEL_SECRET,
+				"Insert your API Key here for maximum precision and application support (Free version is used otherwise).",
 				""));
+		types.add(new ParameterTypeString(API_CHANNEL_USER,
+				"Indicate the API Key domain.", ""));
 		types.add(new ParameterTypeString(
 				DEFAULT_COUNTRY_ISO2,
-				"This parameter defines the default Country ISO2 to use as Locale. Leave blank for a global context. ",
+				"This parameter defines the default Country ISO2 to use as Locale. Leave blank to use the global context (World). ",
 				""));
 		types.add(new ParameterTypeDouble(
 				ATTRIBUTE_THRESHOLD,
-				"This parameter defines the threshold for considering the gender Unknown, ex. -0.10 to 0.10",
+				"This parameter defines the threshold for considering the gender Unknown. Default: 0.10",
 				0, +1, ATTRIBUTE_THRESHOLD_DEFAULT));
 		return types;
 	}
--- src/com/namsor/api/rapidminer/GenderAPIClient.java
+++ src/com/namsor/api/rapidminer/GenderAPIClient.java
@@ -8,71 +8,160 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
 public class GenderAPIClient {
-	
+
 	private static final long batchRequestId = System.currentTimeMillis();
-	private static final String ATTR_XBatchRequest = "X-BatchRequest-Id"; 
-	private static final String ATTR_XClientVersion = "X-Client-Version"; 
+    private static final String ATTR_XMashapeAuthorization="X-Mashape-Authorization";
+    private static final String ATTR_XChannelSecret="X-Channel-Secret";
+    private static final String ATTR_XChannelUser="X-Channel-User";
+	private static final String ATTR_XBatchRequest = "X-BatchRequest-Id";
+	private static final String ATTR_XClientVersion = "X-Client-Version";
 
-	private static final String ATTVALUE_ClientAppVersion = "RapidMinerExt_v0.0.1"; 
+	private static final String MASHAPE_API_ADDRESS = "https://namsor-gendre.p.mashape.com/gendre/";
+
+	private static final String ATTVALUE_ClientAppVersion = "RapidMinerExt_v0.0.1";
+
+	private static final String API_ADDRESS = "http://api.onomatic.com/onomastics/api/gendre/";
+	// private static final String API_ADDRESS =
+	// "http://localhost:8084/onomastics/api/gendre/";
+
+	private static final GenderAPIClient instance = new GenderAPIClient();
+
+	public static GenderAPIClient getInstance() {
+		return instance;
+	}
+
+	public Double genderizeMashape(final String mashapeAPIKey,
+			final String firstName, String lastName, String iso2)
+			throws GenderAPIException {
+		if (firstName == null || firstName.trim().isEmpty() || lastName == null
+				|| lastName.trim().isEmpty()) {
+			return 0d;
+		}
+		try {
+			String url = null;
+			if (iso2 == null) {
+				url = MASHAPE_API_ADDRESS
+						+ URLEncoder.encode(firstName.replace('.', ' ').trim(),
+								"UTF-8")
+						+ "/"
+						+ URLEncoder.encode(lastName.replace('.', ' ').trim(),
+								"UTF-8");
+			} else {
+				url = MASHAPE_API_ADDRESS
+						+ URLEncoder.encode(firstName.replace('.', ' ').trim(),
+								"UTF-8")
+						+ "/"
+						+ URLEncoder.encode(lastName.replace('.', ' ').trim(),
+								"UTF-8") + "/" + iso2;
+			}
+			HttpResponse<String> request = Unirest.get(url)
+					.header(ATTR_XMashapeAuthorization, mashapeAPIKey)
+					.asString();
+			double genderScale = Double.parseDouble(request.getBody());
+			return genderScale;
+		} catch (Exception e) {
+			throw new GenderAPIException(e);
+		}
+	}
+
+	/**
+	 * Predict Gender from a Personal name
+	 * 
+	 * @param firstName
+	 *            The given name
+	 * @param lastName
+	 *            The family name
+	 * @return Double in range -1 (male) .. +1 (female)
+	 */
+	public Double genderizeFree(final String firstName, String lastName,
+			String iso2) throws GenderAPIException {
+		return genderize(null, null, ""+batchRequestId, firstName, lastName, iso2);
+	}
+
+	/**
+	 * Predict Gender from a Personal name
+	 * 
+	 * @param APIChannel
+	 *            The API Channel
+	 * @param APIKey
+	 *            The API Key
+	 * @param firstName
+	 *            The given name
+	 * @param lastName
+	 *            The family name
+	 * @return Double in range -1 (male) .. +1 (female)
+	 */
+	public Double genderizePremium(final String APIChannel, final String APIKey, final String batchId, final String firstName, String lastName,
+			String iso2) throws GenderAPIException {
+		return genderize(APIChannel, APIKey, batchId, firstName, lastName, iso2);
+	}
 	
-    private static final String API_ADDRESS = "http://api.onomatic.com/onomastics/api/gendre/";
-    //private static final String API_ADDRESS = "http://localhost:8084/onomastics/api/gendre/";
+	private Double genderize(final String APIChannel, final String APIKey, final String batchId, final String firstName, String lastName,
+				String iso2) throws GenderAPIException {
+		
+		if (firstName == null || firstName.trim().isEmpty() || lastName == null
+				|| lastName.trim().isEmpty()) {
+			return 0d;
+		}
 
-    private static final GenderAPIClient instance = new GenderAPIClient();
-    
-    public static GenderAPIClient getInstance() {
-    	return instance;
-    }
-    
-    /**
-     * Predict Gender from a Personal name
-     *
-     * @param firstName The given name
-     * @param lastName The family name
-     * @return Double in range -1 (male) .. +1 (female)
-     */
-    public Double genderize(final String firstName, String lastName, String iso2) throws GenderAPIException {
-        if( firstName == null || firstName.trim().isEmpty() || 
-                lastName == null || lastName.trim().isEmpty()
-                ) {
-            return 0d;
-        }
-        
-        String url = null;
-        StringWriter resp = new StringWriter();
-        try {
-            
-            if (iso2 == null) {
-                url = API_ADDRESS
-                        + URLEncoder.encode(firstName.replace('.', ' ').trim(), "UTF-8") + "/"
-                        + URLEncoder.encode(lastName.replace('.', ' ').trim(), "UTF-8");
-            } else {
-                url = API_ADDRESS
-                        + URLEncoder.encode(firstName.replace('.', ' ').trim(), "UTF-8") + "/"
-                        + URLEncoder.encode(lastName.replace('.', ' ').trim(), "UTF-8") + "/" + iso2;
-            }
-        	URL api = new URL(url);
+		String url = null;
+		StringWriter resp = new StringWriter();
+		try {
 
-            HttpURLConnection myURLConnection = (HttpURLConnection)api.openConnection();
-            myURLConnection.setRequestMethod("GET");
-            myURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
-            myURLConnection.setRequestProperty(ATTR_XBatchRequest, ""+batchRequestId);
-            myURLConnection.setRequestProperty(ATTR_XClientVersion, ATTVALUE_ClientAppVersion);
-            
-            BufferedReader in = new BufferedReader(
-            		new InputStreamReader(myURLConnection.getInputStream(),"UTF-8"));
-            String inputLine = in.readLine();
-            while ( inputLine != null) {
-            	resp.append(inputLine);
-            	inputLine = in.readLine();
-            }
-            in.close();        	
-            Double result = Double.parseDouble(resp.toString());
-            return result;
-        } catch (Exception e) {
-        	throw new GenderAPIException(e);
-        }
-    }
+			if (iso2 == null) {
+				url = API_ADDRESS
+						+ URLEncoder.encode(firstName.replace('.', ' ').trim(),
+								"UTF-8")
+						+ "/"
+						+ URLEncoder.encode(lastName.replace('.', ' ').trim(),
+								"UTF-8");
+			} else {
+				url = API_ADDRESS
+						+ URLEncoder.encode(firstName.replace('.', ' ').trim(),
+								"UTF-8")
+						+ "/"
+						+ URLEncoder.encode(lastName.replace('.', ' ').trim(),
+								"UTF-8") + "/" + iso2;
+			}
+			URL api = new URL(url);
+
+			HttpURLConnection myURLConnection = (HttpURLConnection) api
+					.openConnection();
+			myURLConnection.setRequestMethod("GET");
+			myURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
+			if( APIChannel != null ) {
+				myURLConnection.setRequestProperty(ATTR_XChannelUser, APIChannel);				
+			}
+			if( APIKey != null ) {
+				myURLConnection.setRequestProperty(ATTR_XChannelSecret, APIKey);				
+			}
+			if( batchId == null ) {
+				myURLConnection.setRequestProperty(ATTR_XBatchRequest, batchId);				
+			} else {
+				myURLConnection.setRequestProperty(ATTR_XBatchRequest, ""+batchRequestId);								
+			}
+			myURLConnection.setRequestProperty(ATTR_XClientVersion,
+					ATTVALUE_ClientAppVersion);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					myURLConnection.getInputStream(), "UTF-8"));
+			String inputLine = in.readLine();
+			while (inputLine != null) {
+				resp.append(inputLine);
+				inputLine = in.readLine();
+			}
+			in.close();
+			Double result = Double.parseDouble(resp.toString());
+			return result;
+		} catch (Exception e) {
+			throw new GenderAPIException(e);
+		}
+	}
 }
